83f368496455deaefe68ad7a6851676ce1f587a8
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2023-04-25 20:00:03 +0000

    Update install_htop.sh

--- install_htop.sh
+++ install_htop.sh
@@ -1,3 +1,10 @@
+
+#!/usr/bin/env bash
+#
+# Author : Duarte Pinto
+# Date: November 2022
+# Version 1.0.0: "Install Portainer"
+#
 sudo dnf update
 sudo dnf install epel-release
 sudo dnf install htop
